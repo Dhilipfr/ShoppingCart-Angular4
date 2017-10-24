e7a310e8dedf2e1776c68483f733d93203adf0e4
Dhilipfr <dhilipfr@gmail.com>
2017-10-24 18:47:28 +0530

    passing data between components

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,39 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../Shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
+
+    constructor(private slService : ShoppingListService) { }
+
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'Test description recipe', 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/recipes/braised_steak_with_gravy_24508_16x9.jpg'),
-        new Recipe('Another Test Recipe', 'Test description recipe', 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/recipes/braised_steak_with_gravy_24508_16x9.jpg')
+        new Recipe(
+            'Big Fat Burger'
+            , 'What else you need to say?'
+            , 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/recipes/braised_steak_with_gravy_24508_16x9.jpg'
+            , [
+                new Ingredient('test1', 1),
+                new Ingredient('test1', 1)
+            ]
+        ),
+        new Recipe(
+            'Tasty schnitzel'
+            , 'A super-tasty schnitzel - just awesome'
+            , 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/recipes/braised_steak_with_gravy_24508_16x9.jpg'
+            , [
+                new Ingredient('test2',2)
+            ]
+        )
     ];
-
+    
     public getRecipes() {
         return this.recipes.slice();
+    }
+
+    public addToShoppingList(ing: Ingredient[]) {
+        this.slService.addIngredients(ing);
     }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,27 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../Shared/ingredient.model";
+
 export class ShoppingListService {
 
+    public ingredientAdded = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatos', 4)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientAdded.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        debugger;
+        this.ingredients.push(...ingredients);
+        this.ingredientAdded.emit(this.ingredients.slice());
+    }
 }
